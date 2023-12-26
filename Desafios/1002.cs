fc6acce49279f065a23bf543bb6c996b834c9fb1
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-26 11:52:14 -0300

    PROBLEMA: 1002 - Área do Círculo

---
+++ Desafios/1002.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double PI = 3.14159;
+        double raio = Math.Round(double.Parse(Console.ReadLine()), 2);
+        Console.WriteLine($"A={PI * Math.Pow(raio, 2):0.0000}");
+    }
+
+}
