fa397774ff41ac01985493a53757aa3ea94ef441
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-26 17:10:56 -0300

    PROBLEMA: 1008 - Salário

---
+++ Desafios/1008.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int employeeNumber = int.Parse(Console.ReadLine());
+        double employeeHours = double.Parse(Console.ReadLine());
+        double employeeValueHours = double.Parse(Console.ReadLine());
+
+        double salary = employeeHours * employeeValueHours;
+
+        Console.WriteLine($"NUMBER = {employeeNumber}\nSALARY = U$ {salary:F2}");
+
+    }
+
+}
