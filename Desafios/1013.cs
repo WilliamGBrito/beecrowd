c18e74aebcb3306576c67861fe16c94a5ae49d75
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 12:22:44 -0300

    PROBLEMA: 1013 - O Maior

---
+++ Desafios/1013.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] values = Console.ReadLine().Split(' ');
+        int a = int.Parse(values[0]);
+        int b = int.Parse(values[1]);
+        int c = int.Parse(values[2]);
+
+        int maior = a > b && a > c ? a : b > c ? b : c;
+
+        Console.WriteLine($"{maior} eh o maior");
+
+    }
+
+}
