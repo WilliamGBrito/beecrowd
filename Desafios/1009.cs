06ee0b220631335f5208399cabfbcd2d1f451a37
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 10:10:42 -0300

    PROBLEMA: 1009 - Salário com Bônus

---
+++ Desafios/1009.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string name = Console.ReadLine();
+        double salary = double.Parse(Console.ReadLine());
+        double salesAmount = double.Parse(Console.ReadLine());
+
+        double total = 15.0 / 100.0 * salesAmount + salary;
+
+        Console.WriteLine($"TOTAL = R$ {total:F2}");
+
+    }
+
+}
