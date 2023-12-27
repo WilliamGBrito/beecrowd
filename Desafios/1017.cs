38cd509a8aaaea2d5de9e6458490d56e56c9a663
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 17:51:55 -0300

    PROBLEMA: 1017 - Gasto de Combustível

---
+++ Desafios/1017.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int qtdHoras = int.Parse(Console.ReadLine());
+        int velocidadeMedia = int.Parse(Console.ReadLine());
+
+        double qtdLitros = (qtdHoras * velocidadeMedia) / 12.0;
+
+        Console.WriteLine($"{qtdLitros:F3}");
+
+    }
+
+}
