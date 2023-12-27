266cb6cb9bd45a8cf6872030ff0810ce36143ed7
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 16:57:42 -0300

    PROBLEMA: 1016 - Distância

---
+++ Desafios/1016.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int distancia = int.Parse(Console.ReadLine());
+
+        int qtdMinutos = (60 * distancia) / 30;
+        Console.WriteLine($"{qtdMinutos} minutos");
+
+    }
+
+}
