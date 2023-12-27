a6a1d5657e0a5a7eb882bf070c52f55fd3f11423
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 11:16:27 -0300

    PROBLEMA: 1011 - Esfera

---
+++ Desafios/1011.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double raio = double.Parse(Console.ReadLine());
+
+        double esfera = (4 / 3.0) * 3.14159 * (Math.Pow(raio, 3.0));
+
+        Console.WriteLine($"VOLUME = {esfera:F3}");
+
+    }
+
+}
