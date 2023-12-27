457d3f337329e4df9d2e38ed5dd95ce2ac9ad2dc
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 11:41:33 -0300

    PROBLEMA: 1012 - Área

---
+++ Desafios/1012.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] valores = Console.ReadLine().Split(' ');
+        double A = double.Parse(valores[0]);
+        double B = double.Parse(valores[1]);
+        double C = double.Parse(valores[2]);
+
+        double triangulo = A * C / 2.0;
+        double circulo = 3.14159 * C * C;
+        double trapezio = (A + B) / 2.0 * C;
+        double quadrado = B * B;
+        double retangulo = A * B;
+
+        Console.WriteLine($"TRIANGULO: {triangulo:F3}");
+        Console.WriteLine($"CIRCULO: {circulo:F3}");
+        Console.WriteLine($"TRAPEZIO: {trapezio:F3}");
+        Console.WriteLine($"QUADRADO: {quadrado:F3}");
+        Console.WriteLine($"RETANGULO: {retangulo:F3}");
+
+    }
+
+}
