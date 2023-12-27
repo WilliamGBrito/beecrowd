b6dfe87f9e50a05c656e9f6ed8bf7e783dd59179
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 14:34:29 -0300

    PROBLEMA: 1014 - Consumo

---
+++ Desafios/1014.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int X = int.Parse(Console.ReadLine());
+        double Y = double.Parse(Console.ReadLine());
+
+        double consumoMedio = X / Y;
+
+        Console.WriteLine($"{consumoMedio:F3} km/l");
+
+    }
+
+}
