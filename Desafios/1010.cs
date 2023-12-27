4c3d9a93329eadd42cf487084af30ad7f784ef4e
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-27 11:10:40 -0300

    PROBLEMA: 1010 - Cálculo Simples

---
+++ Desafios/1010.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] prod1 = Console.ReadLine().Split(' ');
+        int num1 = int.Parse(prod1[1]);
+        double val1 = double.Parse(prod1[2]);
+
+        string[] prod2 = Console.ReadLine().Split(' ');
+        int num2 = int.Parse(prod2[1]);
+        double val2 = double.Parse(prod2[2]);
+
+        double total = num1 * val1 + num2 * val2;
+        Console.WriteLine($"VALOR A PAGAR: R$ {total:F2}");
+
+    }
+
+}
