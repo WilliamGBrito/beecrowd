75d55a9d4040b1fef0609a6692c97dfe77f4520c
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-28 17:18:21 -0300

    PROBLEMA: 1019 - Conversão de Tempo

---
+++ Desafios/1019.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int qtdSegundos = int.Parse(Console.ReadLine());
+
+        int horas = qtdSegundos / 3600;
+        qtdSegundos = qtdSegundos % 3600;
+
+        int minutos = qtdSegundos / 60;
+        qtdSegundos = qtdSegundos % 60;
+
+        int segundos = qtdSegundos;
+
+        Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+    }
+
+}
