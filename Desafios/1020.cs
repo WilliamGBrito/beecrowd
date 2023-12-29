43cf85c920be3671b45fc704dbb0d1e049013d42
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-29 10:34:15 -0300

    PROBLEMA: 1020 - Idade em Dias

---
+++ Desafios/1020.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int qtdDias = int.Parse(Console.ReadLine());
+
+        int ano = qtdDias / 365;
+        qtdDias = qtdDias % 365;
+
+        int mes = qtdDias / 30;
+        qtdDias = qtdDias % 30;
+
+        int dia = qtdDias;
+
+        Console.WriteLine($"{ano} ano(s)\n{mes} mes(es)\n{dia} dia(s)");
+
+    }
+
+}
